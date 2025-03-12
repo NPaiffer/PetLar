7630482df1136d2c272dc075e4857d9c68fddde4
NPaiffer <nicpaiffer@hotmail.com>
2025-03-12 11:58:43 -0300

    feat: add project

---
+++ src/main/java/com/petlar/PetLarApplication.java
@@ -0,0 +1,13 @@
+package com.petlar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetLarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetLarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petlar/PetLarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petlar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetLarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
